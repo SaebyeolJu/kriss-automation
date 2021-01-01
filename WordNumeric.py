1923514253b99567cdc65c4729d39ad087cf2067
SB J <>
2021-01-01 23:02:15 +0900

    WordNumeric - word tagging 완료

--- WordNumeric.py
+++ WordNumeric.py
@@ -1,6 +1,7 @@
 # 2021/01/01
 # Tag 된 단어들 Numeric data로 만들기
 # 엑셀은 WordTag에서 만들어진 엑셀을 사용해 미리 포맷되어 있어야함(TextJoin)
+# 짝 묶어주고, Numeric 짝 없는건 x 채우는건 수동
 
 import openpyxl
 import re
@@ -22,6 +23,8 @@ synthesis = ['s']
 
 number = ['n', 'mn']
 metric = ['u', 'mu']
+
+category_rule = {'0': material, '1': device, '2': performance, '3': mechanism, '4': environment, '5': synthesis}
 # number_trim
 trim = re.compile("[^0-9]")
 
@@ -29,7 +32,6 @@ trim = re.compile("[^0-9]")
 AB_DIR = '/Users/SBJ/Desktop/kriss/Data/Text/Word'
 DIR = 'Data/Text/Word/'
 
-print("start")
 # Data/Text/Word 에 있는 파일을 리스트로 생성
 file_list = [f for f in listdir(AB_DIR) if isfile(join(AB_DIR, f))]
 print(file_list)
@@ -41,8 +43,7 @@ max_row = sheet.max_row
 max_col = sheet.max_column
 
 for row in range(3, max_row + 1, 2):
-    # print(row)
-    numerical = []
+    numerical, numerical_temp = [], []
     category = [0] * 6
 
     for col in range(4, max_col + 1):
@@ -52,26 +53,11 @@ for row in range(3, max_row + 1, 2):
         cell_info = ("".join(trim.findall(sheet.cell(row, col).value)))
 
         if cell_info in number or cell_info in metric:
-            numerical.append(sheet.cell(row-1, col).value)
-        elif cell_info in material:
-            category[0] += 1
-            break
-        elif cell_info in device:
-            category[1] += 1
-            break
-        elif cell_info in performance:
-            category[2] += 1
-            break
-        elif cell_info in mechanism:
-            category[3] += 1
-            break
-        elif cell_info in environment:
-            category[4] += 1
-            break
-        elif cell_info in synthesis:
-            category[5] += 1
-
-
+            numerical_temp.append(sheet.cell(row-1, col).value)
+            if not len(numerical_temp) % 2 : numerical.append(numerical_temp)
+        for i in range(0, len(category_rule)):
+            if cell_info in category_rule[str(i)] : category[i] += 1
+    if len(numerical_temp) % 2 : numerical.append(numerical_temp + ['x'])
     sheet.cell(row, 2).value = str(category)
     sheet.cell(row, 3).value = str(numerical)
 
